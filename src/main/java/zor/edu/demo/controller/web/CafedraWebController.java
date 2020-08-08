dc65f890197342603705c56949de7eba96ae2071
StelIify <morf3310@gmail.com>
2020-08-08 12:51:29 +0200

    working web controller

--- src/main/java/zor/edu/demo/controller/web/CafedraWebController.java
+++ src/main/java/zor/edu/demo/controller/web/CafedraWebController.java
@@ -1,14 +1,25 @@
 package zor.edu.demo.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zor.edu.demo.service.cafedra.impls.CafedralServiceImpl;
 
 @Controller
 @RequestMapping("/web/cafedra")
 public class CafedraWebController
 {
+    @Autowired
+    CafedralServiceImpl cafedralService;
+
     @RequestMapping("get/list")
-    String showAll(){
+    String showAll(Model model)
+    {
+        model.addAttribute("list", cafedralService.getAll());
         return "cafedralist";
+
     }
+
+
 }
